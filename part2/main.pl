6c2bffedb25f7bc599fc437b42616c054724f503
Davide B <davide1.bonaventura@gmail.com>
2023-05-03 11:23:52 +0100

    edit yap2mysql in yap2postgres

--- part2/main.pl
+++ part2/main.pl
@@ -1,8 +1,8 @@
-:- consult(yap2mysql).
-:- db_open(localhost, davide, 'davide', tangram_puzzle, conn).
-:- db_import(pieces, pieces, conn).
-:- db_import(puzzles, puzzles, conn).
-:- db_import(solutions, solutions, conn).
+:- consult(yap2postgres).
+:- db_open('192.168.175.132', davide, 'davide', tangram_puzzle, conn).
+% :- db_import(pieces, pieces, conn).
+% :- db_import(puzzles, puzzles, conn).
+% :- db_import(solutions, solutions, conn).
 
 % :- get_value(conn, ConnHandler),db_arity(ConnHandler, pieces, Arity).
-:- db_close(conn).
+% :- db_close(conn).
---
+++ part2/yap2postgres.c
@@ -0,0 +1,181 @@
+#include "Yap/YapInterface.h"
+#include <libpq-fe.h>
+
+void init_predicates();
+static bool finish_with_error(PGconn*, PGresult*); 
+
+static int c_db_connect(void);
+static int c_db_disconnect(void);
+// static int c_db_assert(void);
+static int c_db_query(void);
+static int c_db_arity(void);
+
+static int c_db_row_start(void);
+static int c_db_row_continue(void);
+static int c_db_row_cut(void);
+
+typedef struct {
+    YAP_Term current_row;
+} c_db_row_type;
+
+c_db_row_type *c_db_row_data;
+
+void init_predicates() {
+    YAP_UserCPredicate("db_connect" , c_db_connect, 5);
+    YAP_UserCPredicate("db_disconnect" , c_db_disconnect, 1);
+    // YAP_UserCPredicate("db_assert" , c_db_assert, 3);
+    YAP_UserCPredicate("db_query" , c_db_query, 3);
+    YAP_UserBackCutCPredicate("db_row", c_db_row_start, c_db_row_continue, c_db_row_cut, 2, 1);
+
+    YAP_UserCPredicate("db_arity" , c_db_arity, 3);
+}
+
+static bool finish_with_error(PGconn *conn, PGresult *res)
+{
+    fprintf(stderr, "%s\n", PQerrorMessage(conn));
+    if (res != NULL)
+        PQclear(res);
+    PQfinish(conn);
+	return FALSE;
+}
+
+
+static int c_db_connect(void) {
+    YAP_Term 
+        arg_host    = YAP_ARG1,
+        arg_user    = YAP_ARG2,
+        arg_passwd  = YAP_ARG3,
+        arg_database= YAP_ARG4,
+        arg_conn    = YAP_ARG5;
+
+    char 
+        *host   = YAP_AtomName(YAP_AtomOfTerm(arg_host)),
+        *user   = YAP_AtomName(YAP_AtomOfTerm(arg_user)),
+        *passwd = YAP_AtomName(YAP_AtomOfTerm(arg_passwd)),
+        *database = YAP_AtomName(YAP_AtomOfTerm(arg_database));
+
+    char connectionInfo[1024];
+    snprintf(
+        connectionInfo, 
+        sizeof(connectionInfo), 
+        "user=%s password=%s dbname=%s host=%s port=5432", 
+        user, passwd, database, host
+    );
+
+    PGconn *conn = PQconnectdb(connectionInfo);
+    if (PQstatus(conn) != CONNECTION_OK)
+    {
+        fprintf(stderr, "Connection to database failed...\n");
+        finish_with_error(conn, NULL);
+    }
+
+    if(!YAP_Unify(arg_conn, YAP_MkIntTerm((long)conn))){
+        return FALSE;
+    }
+    return TRUE;
+}
+
+static int c_db_disconnect(void) {
+    YAP_Term arg_conn = YAP_ARG1;
+    
+    PGconn *conn = (PGconn *) YAP_IntOfTerm(arg_conn);
+
+    PQfinish(conn);
+    return TRUE;
+}
+
+static int c_db_query(void) {
+    YAP_Term 
+        arg_conn        = YAP_ARG1,
+        arg_sql         = YAP_ARG2,
+        arg_result_set  = YAP_ARG3;
+    
+    PGconn *conn = (PGconn*) YAP_IntOfTerm(arg_conn);
+    char *sql = YAP_AtomName(YAP_AtomOfTerm(arg_sql));
+    
+    PGresult *result_set = PQexec(conn, sql);
+    if (PQresultStatus(result_set) != PGRES_TUPLES_OK)
+    {
+        fprintf(stderr, "Error executing query : %s\n", sql);
+        finish_with_error(conn, result_set);
+    }
+
+    if(!YAP_Unify(arg_result_set, YAP_MkIntTerm((long) result_set))){
+        return FALSE;
+    }
+
+    return TRUE;
+}
+
+static int c_db_row_start(void) {
+    YAP_PRESERVE_DATA(c_db_row_data, c_db_row_type);
+    c_db_row_data->current_row = YAP_MkIntTerm(0);
+
+    return c_db_row_continue();
+}
+
+static int c_db_row_continue(void) {
+    PGresult *res_set;
+    int i, arity, n_rows;
+    char *value;
+
+    YAP_PRESERVED_DATA(c_db_row_data, c_db_row_type);
+    i = YAP_IntOfTerm(c_db_row_data->current_row);
+
+    res_set = (PGresult*) YAP_IntOfTerm(YAP_ARG1);
+    arity = PQnfields(res_set);
+    n_rows = PQntuples(res_set);
+
+    if (i < n_rows) {
+        YAP_Term head, list = YAP_ARG2;
+        for (int j = 0; j < arity; j++) {
+            head = YAP_HeadOfTerm(list);
+            list = YAP_TailOfTerm(list);
+            value = PQgetvalue(res_set, i, j);
+            if(!YAP_Unify(head, YAP_MkAtomTerm(YAP_LookupAtom(value ? value : "NULL")))) {
+                return FALSE;
+            }
+        }
+        c_db_row_data->current_row = YAP_MkIntTerm(i+1);
+        return TRUE;
+    }
+
+    YAP_cut_fail();
+}
+
+static int c_db_row_cut(void) {
+    PGresult *res_set = (PGresult*) YAP_IntOfTerm(YAP_ARG1);
+    PQclear(res_set);
+    return TRUE;
+}
+
+// db_arity(ConnHandler, RelationName, Arity),
+static int c_db_arity(void) {
+    YAP_Term 
+        arg_conn    = YAP_ARG1,
+        arg_rel_name     = YAP_ARG2,
+        arg_arity   = YAP_ARG3;
+    
+    PGconn *conn = (PGconn*) YAP_IntOfTerm(arg_conn);
+    char 
+        *rel_name = YAP_AtomName(YAP_AtomOfTerm(arg_rel_name)),
+        *sql = (char*)malloc(1024 * sizeof(char));
+    int arity;
+
+    sprintf(sql, "select * from %s limit 0", rel_name);
+    PGresult *res = PQexec(conn, sql);
+    if (PQresultStatus(res) != PGRES_TUPLES_OK) {
+        fprintf(stderr, "Error executing query : %s\n", sql);
+        finish_with_error(conn, res);
+    }
+
+    arity = PQnfields(res);
+
+    if(!YAP_Unify(arg_arity, YAP_MkIntTerm((int) arity))){
+        return FALSE;
+    }
+
+    free(sql);
+    PQclear(res);
+    return TRUE;
+}

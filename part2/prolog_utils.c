5f73e94610443ddd1ef2cce34a35f2ee81333bf3
IgnazioGul <ignazio.gulino@aitho.it>
2023-05-07 18:52:18 +0200

    fix: fix st_difference

--- part2/prolog_utils.c
+++ part2/prolog_utils.c
@@ -7,8 +7,11 @@ void init_predicates()
      YAP_UserCPredicate("st_difference", c_st_difference, 4);
 }
 
-// st_diff(conn, geom1, geom2, res).
+/**
+ * TEST st_difference -> consult(yap2postgres),db_open(localhost, postgres, 'gnazio', tangram_puzzle, conn),get_value(conn, ConnHandler),st_difference(ConnHandler, [[0.0000000000000000,0.0000000000000000,0.5000000000000000,0.5000000000000000,1.0000000000000000,0.0000000000000000,0.0000000000000000,0.0000000000000000]], [[0.0000000000000000,0.0000000000000000,0.5000000000000000,0.0000000000000000,0.2500000000000000,0.2500000000000000,0.0000000000000000,0.0000000000000000]], A).
+*/
 
+// st_diff(conn, geom1, geom2, res).
 static int c_st_difference(void) 
 {
     PGconn *conn = (PGconn *)YAP_IntOfTerm(YAP_ARG1);
@@ -18,7 +21,6 @@ static int c_st_difference(void)
 
     char *f1_WKL = extract_WKT_from_points(conn, f1);
     char *f2_WKL = extract_WKT_from_points(conn, f2);
-
     char sql[1024];
     sprintf(sql, "select ST_Difference(ST_GeomFromText('%s'), ST_GeomFromText('%s'))", f1_WKL, f2_WKL);
     PGresult *res = PQexec(conn, sql);
@@ -27,32 +29,13 @@ static int c_st_difference(void)
         fprintf(stderr, "Error executing query : %s\n", sql);
         finish_with_error(conn, res);
     }
-    if (!YAP_Unify(diff_res, YAP_MkIntTerm((long)PQgetvalue(res, 0, 0))))
+    char *res_str = PQgetvalue(res, 0, 0);
+    sprintf(sql, "SELECT ST_NumInteriorRings('%s')", res_str);
+    int n_int_rings = atoi(PQgetvalue(exec_sql(conn, sql), 0, 0)); // num of interior rings, add 1 to get the total amount of rings
+    YAP_Term res_term = extract_values(conn, res_str,n_int_rings);
+    if (!YAP_Unify(diff_res, res_term))
     {
         return FALSE;
     }
     return TRUE;
 }
-
-
-/* static int c_st_diff(void) 
-{
-    PGconn *conn = (PGconn *)YAP_IntOfTerm(YAP_ARG1);
-    char *f1 = YAP_AtomName(YAP_AtomOfTerm(YAP_ARG2));
-    char *f2 = YAP_AtomName(YAP_AtomOfTerm(YAP_ARG3));
-    YAP_Term diff_res = YAP_ARG4;
-
-    char sql[1024];
-    sprintf(sql, "select ST_Difference(ST_GeomFromText('%s'), ST_GeomFromText('%s'))", f1, f2);
-    PGresult *res = PQexec(conn, sql);
-    if (PQresultStatus(res) != PGRES_TUPLES_OK)
-    {
-        fprintf(stderr, "Error executing query : %s\n", sql);
-        finish_with_error(conn, res);
-    }
-    if (!YAP_Unify(diff_res, YAP_MkIntTerm((long)PQgetvalue(res, 0, 0))))
-    {
-        return FALSE;
-    }
-    return TRUE;
-} */

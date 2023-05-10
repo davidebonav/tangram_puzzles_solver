7b82035ad2dd1b544dcac0993572a3c9d33deaa9
Davide B <davide1.bonaventura@gmail.com>
2023-05-10 01:34:39 +0100

    edit tangram.pl

--- part4/tangram.pl
+++ part4/tangram.pl
@@ -1,8 +1,27 @@
 consult(../part2/yap2postgres).
-init :-  db_open(localhost, postgres, 'gnazio', tangram_puzzle, ConnName),
-            db_import(pieces, pieces, ConnName),
-            db_import(puzzles, puzzles, ConnName),
-            db_import(solutions, solutions, ConnName).
+
+init(ConnName) :-  
+        db_open('192.168.175.132', davide, 'davide', tangram_puzzle, ConnName),
+        db_import(pieces, pieces, ConnName),
+        db_import(puzzles, puzzles, ConnName),
+        db_import(solutions, solutions, ConnName).
+ 
+solve_tangram(PuzzleId, Solution, ConnName) :- 
+        get_value(ConnName, ConnHandler),
+        loadData(PuzzleId, PuzzleShape, PiecesList),
+        solve_tangram_helper(PiecesList, PuzzleShape, ConnHandler).
+
+loadData(PuzzleId, PuzzleShape, PiecesList) :- 
+        findall(PieceShape,(pieces(_, _, _, PieceShape)), PiecesList),
+        puzzles(PuzzleId, _, PuzzleShape).
+
+solve_tangram_helper([], _, ConnHandler).
+solve_tangram_helper([Piece|Rest], Goal, ConnHandler) :-
+    pieces(_, PieceName, PieceColor, PieceCoords),
+    rotate_and_translate(PieceCoords, TransformedPieceCoords, ConnHandler),
+    member(PieceCoords, TransformedPieceCoords),
+    subtract(ConnHandler, Goal, PieceCoords, RemainingGoal),
+    solve_tangram_helper(Rest, RemainingGoal,ConnHandler).
 
 % Predicato per ruotare le coordinate del pezzo
 rotate_piece(PieceCoords, RotatedPieceCoords, ConnHandler) :-
@@ -20,26 +39,3 @@ translate_piece(PieceCoords, TransformedPieceCoords, ConnHandler) :-
 rotate_and_translate(PieceCoords, TransformedPieceCoords, ConnHandler) :-
     rotate_piece(PieceCoords, RotatedPieceCoords, ConnHandler),
     translate_piece(RotatedPieceCoords, TransformedPieceCoords, ConnHandler).
-
-solve_tangram_helper([], _, conn).
-solve_tangram_helper([Piece|Rest], Goal, ConnHandler) :-
-    pieces(_, PieceName, PieceColor, PieceCoords),
-    rotate_and_translate(PieceCoords, TransformedPieceCoords, ConnHandler),
-    member(PieceCoords, TransformedPieceCoords),
-    subtract(ConnHandler, Goal, PieceCoords, RemainingGoal),
-    solve_tangram_helper(Rest, RemainingGoal).
-
-loadData(Pieces, Puzzle) :- findall((PieceType, Color, PieceShape), pieces(PieceType, Color, PieceShape), Pieces),
-                            clause(puzzles(Id, PuzzleNname, PuzzleShape), true),  % find first fact
-                            Puzzle = puzzles(Id, PuzzleNname, PuzzleShape).
-
-solve_tangram :- loadData(Pieces, Puzzle, ConnName),
-                    get_value(ConnName, ConnHandler),
-                    solve_tangram_helper(Pieces, Puzzle, ConnHandler).
-
-
-
-
-
-
-

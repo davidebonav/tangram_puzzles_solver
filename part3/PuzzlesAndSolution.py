3831606c4e7c0db588166f89b178344f7f0a1af5
Davide B <davide1.bonaventura@gmail.com>
2023-05-16 16:42:44 +0100

    add PuzzlesAndSolution in part3

---
+++ part3/PuzzlesAndSolution.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python3
+
+from utils import db_row_to_patch
+from SolutionsTable import SolutionsTable
+
+import matplotlib.pyplot as plt
+
+class PuzzlesAndSolution:
+    def __init__(self, conn):
+        self.conn = conn
+
+        self.get_puzzles()
+        self.get_solutions()
+
+        cur = self.conn.cursor()
+        cur.execute('SELECT color FROM pieces ORDER BY id;')
+        rows = cur.fetchall()
+        cur.close()
+        self.colors = [row[0] for row in rows]
+
+    def get_puzzles(self):
+        sql = "SELECT * FROM puzzles ORDER BY id"
+        cur = self.conn.cursor()
+        cur.execute(sql)
+        self.puzzles = cur.fetchall()
+        cur.close()
+
+    def get_solutions(self):
+        tmp = SolutionsTable(self.conn)
+        sql = tmp.select_star()
+        cur = self.conn.cursor()
+        cur.execute(sql)
+        self.solutions = cur.fetchall()
+        cur.close()
+
+    def plot_table(self):
+
+        ncols=len(self.puzzles)
+        nrows=2
+
+        plt.rcParams["figure.autolayout"] = True
+
+        fix, axes = plt.subplots(nrows=nrows, ncols=ncols)
+        # if not self.is_matrix(axes):
+        #     axes = [axes,[]]
+
+        fix.set_size_inches(15, 8)
+
+        # print all puzzle
+        for i in range(ncols):
+            puzzle = self.puzzles[i]
+            ax = axes[0][i]
+
+            ax.clear()
+            ax.set_title(f"id={puzzle[0]}, name={puzzle[1]}")
+            patch = db_row_to_patch(
+                    points=list(puzzle[2].exterior.coords))
+            ax.add_patch(patch)
+            ax.set_xlim(-0.3, 1.3)
+            ax.set_ylim(-0.1, 1.3)
+            ax.set_aspect("equal")
+
+            plt.pause(0.005)
+        
+        # print all solution
+        for i in range(ncols):
+            solution = self.solutions[i]
+            ax = axes[1][i]
+
+            ax.clear()
+            ax.set_title(f"puzzle {solution[0]} solution")
+            for k in range(1,len(solution)):
+                patch = db_row_to_patch(
+                    points=list(solution[k].exterior.coords), 
+                    color=self.colors[k-1])
+                ax.add_patch(patch)
+            ax.set_xlim(-0.3, 1.3)
+            ax.set_ylim(-0.1, 1.3)
+            ax.set_aspect("equal")
+
+            plt.pause(0.005)
+
+        plt.tight_layout()
+        plt.show()
--- part3/main.py
+++ part3/main.py
@@ -5,6 +5,7 @@ from utils import register_geometry_type
 from PiecesTable import *
 from PuzzlesTable import *
 from SolutionsTable import *
+from PuzzlesAndSolution import *
 
 ascii_art = """
  ______   ____  ____    ____  ____    ____  ___ ___     
@@ -29,14 +30,15 @@ menu_1 = """
 --> 1. pieces table
 --> 2. puzzles table
 --> 3. solutions table
---> 4. exit
+--> 4. plot puzzles and solutions
+--> 0. exit
 -> """
 
 menu_2 = """
 -> Select an Option:
 --> 1. print table
 --> 2. plot table
---> 3. exit
+--> 0. exit
 -> """
 
 def safe_exit(conn):
@@ -65,6 +67,10 @@ def main():
         elif choise == '3':
             table = SolutionsTable(conn)
         elif choise == '4':
+            tmp = PuzzlesAndSolution(conn)
+            tmp.plot_table()
+            continue
+        elif choise == '0':
             safe_exit(conn)
         else:
             print("-> Please, select a valid option...")
@@ -75,32 +81,11 @@ def main():
             table.print_table()
         elif choise == '2':
             table.plot_table()
-        elif choise == '3':
+        elif choise == '0':
             safe_exit(conn)
         else:
             print("-> Please, select a valid option...")
             continue
-
-
-
-    # print("Pieces table: ")
-    # PiecesTable.print_table(conn)
-    # print("\n\nPuzzles table:")
-    # PuzzlesTable.print_table(conn)
-    # print("\n\nSolutions table:")
-    # SolutionsTable.print_table(conn)
-
-    # pieces = PiecesTable(conn)
-    # pieces.plot_table()
-    # puzzles = PuzzlesTable(conn)
-    # puzzles.plot_table()
-    # solutions = SolutionsTable(conn)
-    # solutions.plot_table()
-
-    # puzzle = PiecesTable(conn)
-    # puzzle.solve_puzzle()
-
-    # Close the connection
 
 if __name__ == "__main__":
     register_geometry_type()

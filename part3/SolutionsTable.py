ad8205ea1621ea4d07f3cd0b69535d62bb013ca7
IgnazioGul <ignazio.gulino@aitho.it>
2023-05-23 19:03:05 +0200

    fix: fix solutions plotting

--- part3/SolutionsTable.py
+++ part3/SolutionsTable.py
@@ -2,6 +2,7 @@
 
 from Table import Table
 from utils import plot_polygon
+from shapely import wkb
 
 class SolutionsTable(Table):
 
@@ -54,7 +55,8 @@ class SolutionsTable(Table):
 
     def set_ax_patches(self,ax,row):
         for i in range(1,len(row)):
-            plot_polygon(ax, row[i], 
+            row2 = [wkb.loads(row[idx], hex=True) if idx != 0 else row[idx] for idx, val in enumerate(row)]
+            plot_polygon(ax, row2[i], 
                 color=self.colors[i-1],
                 alpha=0.9
             )
